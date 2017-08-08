d4ba2cf09a8efdc16e07ac6f6b96634fa9494e53
Rafi Dudekula <rafi.dudekula@gmail.com>
2017-08-08 12:37:54 -0500

    small change for testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AssetBuilder12' });
+  res.render('index', { title: 'AssetBuilder123' });
 });
 
 module.exports = router;
