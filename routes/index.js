4d8842736383537b9a14146f6b717a91e5220dd4
Rafi Dudekula <rafi.dudekula@gmail.com>
2017-08-08 12:30:18 -0500

    added nodemon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AssetBuilder' });
+  res.render('index', { title: 'AssetBuilder12' });
 });
 
 module.exports = router;
